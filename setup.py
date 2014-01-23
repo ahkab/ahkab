cb5fb5ba001ec8ca7d3fb686a003c938cebec269
ggventurini <ggventurini+GITHUB@gmail.com>
2014-01-23 19:20:30 +0100

    General improvements.

--- setup.py
+++ setup.py
@@ -1,23 +1,44 @@
 #!/usr/bin/env python
-from distutils.core import setup
-from ahkab.ahkab import __version__
+import os
+from setuptools import setup, find_packages
+__version__ = "0.09"
 
-setup(name='ahkab',
-      version=__version__,
-      description='Ahkab: a SPICE-like electronic circuit simulator',
-      author='Giuseppe Venturini and others',
-      author_email='ggventurini+GITHUB@gmail.com',
-      url='http://ahkab.github.io/ahkab/',
-      packages=['ahkab']
-     )
+def read(fname):
+    try:
+        with open(os.path.join(os.path.dirname(__file__), fname)) as fp:
+            return fp.read()
+    except IOError:
+        return ""
 
-print """
-+---------------------------------------------------+
-The following dependencies are needed to run ahkab:
-- numpy: http://numpy.scipy.org/ 
-- sympy: http://code.google.com/p/sympy/
-- matplotlib: http://matplotlib.sourceforge.net/
+setup(
+    name='deltasigma',
+    version=__version__,
+    packages=find_packages(),
+    package_data={
+      'ahkab': ['test_data/*.dat']
+    },
+    install_requires=['numpy', 'sympy', 'matplotlib>=1.1.1'],
+    zip_safe=False,
+    include_package_data=True,
+    author="Giuseppe Venturini and others",
+    author_email="ggventurini+GITHUB@gmail.com",
+    description="a SPICE-like electronic circuit simulator",
+    long_description=''.join([read('pypi_description.rst'), '\n\n',
+                              read('CHANGES.rst')]),
+    license="GPL",
+    keywords="electronic circuit simulator numeric symbolic",
+    url='http://ahkab.github.io/ahkab/',
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: POSIX",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: MacOS",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7"]
+)
 
-They are available through PyPi. (See Install.md)
-+---------------------------------------------------+
-"""
