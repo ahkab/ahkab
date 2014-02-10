d51c04abb847949491242d7b7f429793ce416bc9
weilawei <weilawei@gmail.com>
2014-02-09 20:15:05 -0500

    Savegame, probably buggy

--- ahkab/utilities.py
+++ ahkab/utilities.py
@@ -292,7 +292,7 @@ def Kelvin2Celsius(kel):
 #   Maximum attenuation pass band/Minimum attenuation stop band
 def MAPSB(results, pass_band, stop_band):
     # Normalize the output to the low frequency value and convert to array
-    norm_out = numpy.asarray(results['|Vn4|'].T/r['|Vn4|'].max())
+    norm_out = numpy.asarray(results['|Vn4|'].T/results['|Vn4|'].max())
     
     # Convert to dB
     norm_out_db = 20 * numpy.log10(norm_out)
@@ -301,7 +301,7 @@ def MAPSB(results, pass_band, stop_band):
     norm_out_db = norm_out_db.reshape((max(norm_out_db.shape),))
     
     # Convert angular frequencies to Hz and convert matrix to array
-    frequencies = numpy.asarray(r['w'].T/2/math.pi)
+    frequencies = numpy.asarray(results['w'].T/2/math.pi)
     
     # Reshape to be scipy-friendly
     frequencies = frequencies.reshape((max(frequencies.shape),))
