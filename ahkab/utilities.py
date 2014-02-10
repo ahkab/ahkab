eb037b1d078509096e36f30fe46e7243f335da42
weilawei <weilawei@gmail.com>
2014-02-09 19:45:01 -0500

    MAPSB for fork

--- ahkab/utilities.py
+++ ahkab/utilities.py
@@ -1,3 +1,25 @@
+
+#   Maximum attenuation pass band/Minimum attenuation stop band
+def MAPSB(results, pass_band, stop_band):
+    # Normalize the output to the low frequency value and convert to array
+    norm_out = numpy.asarray(results['|Vn4|'].T/r['|Vn4|'].max())
+    
+    # Convert to dB
+    norm_out_db = 20 * numpy.log10(norm_out)
+    
+    # Reshape to be scipy-friendly
+    norm_out_db = norm_out_db.reshape((max(norm_out_db.shape),))
+    
+    # Convert angular frequencies to Hz and convert matrix to array
+    frequencies = numpy.asarray(r['w'].T/2/math.pi)
+    
+    # Reshape to be scipy-friendly
+    frequencies = frequencies.reshape((max(frequencies.shape),))
+    
+    # call scipy to interpolate
+    norm_out_db_interpolated = scipy.interpolate.interp1d(frequencies, norm_out_db)
+
+    return (-1.0*norm_out_db_interpolated(pass_band), -1.0*norm_out_db_interpolated(stop_band))
 # -*- coding: iso-8859-1 -*-
 # utilities.py
 # Utilities file for Ahkab simulator
@@ -266,3 +288,25 @@ def Celsius2Kelvin(cel):
 
 def Kelvin2Celsius(kel):
     return kel - 273.15
+
+#   Maximum attenuation pass band/Minimum attenuation stop band
+def MAPSB(results, pass_band, stop_band):
+    # Normalize the output to the low frequency value and convert to array
+    norm_out = numpy.asarray(results['|Vn4|'].T/r['|Vn4|'].max())
+    
+    # Convert to dB
+    norm_out_db = 20 * numpy.log10(norm_out)
+    
+    # Reshape to be scipy-friendly
+    norm_out_db = norm_out_db.reshape((max(norm_out_db.shape),))
+    
+    # Convert angular frequencies to Hz and convert matrix to array
+    frequencies = numpy.asarray(r['w'].T/2/math.pi)
+    
+    # Reshape to be scipy-friendly
+    frequencies = frequencies.reshape((max(frequencies.shape),))
+    
+    # call scipy to interpolate
+    norm_out_db_interpolated = scipy.interpolate.interp1d(frequencies, norm_out_db)
+
+    return (-1.0*norm_out_db_interpolated(pass_band), -1.0*norm_out_db_interpolated(stop_band))
