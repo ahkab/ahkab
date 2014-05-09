cce368064026ee20efd7aff6feb0bfeb5c8e8cad
ggventurini <ggventurini+GITHUB@gmail.com>
2014-05-09 14:59:13 +0200

    Matplotlib backend selection for machines without localhost:10.0

--- ahkab/__init__.py
+++ ahkab/__init__.py
@@ -1,1 +1,10 @@
+# Package testing can be done remotely, without display. This would make
+# matplotlib fail (and, sometimes, the test as well).
+# If we check for $DISPLAY, that makes us probably lose in portability,
+# because does Windows have the DISPLAY env variable defined?
+import os
+import matplotlib
+if not os.system('python -c "import matplotlib.pyplot as plt;plt.figure()"'):
+    matplotlib.use('Agg')
+
 from . import *
