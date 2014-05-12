a3f7e4ef32f643ed34e9d1858969c4ac1f6bf199
ggventurini <ggventurini+GITHUB@gmail.com>
2014-05-12 11:32:28 +0200

    Fix headless discovery.

--- ahkab/__init__.py
+++ ahkab/__init__.py
@@ -1,10 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: iso-8859-1 -*-
+# ahkab.py
+# The frontend of the simulator
+# Copyright 2006 Giuseppe Venturini
+
+# This file is part of the ahkab simulator.
+#
+# Ahkab is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 2 of the License.
+#
+# Ahkab is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License v2
+# along with ahkab.  If not, see <http://www.gnu.org/licenses/>.
+
+""" ahkab is an easy electronic circuit simulator.
+"""
+
 # Package testing can be done remotely, without display. This would make
 # matplotlib fail (and, sometimes, the test as well).
 # If we check for $DISPLAY, that makes us probably lose in portability,
 # because does Windows have the DISPLAY env variable defined?
 import os
 import matplotlib
-if not os.system('python -c "import matplotlib.pyplot as plt;plt.figure()"'):
+if os.system('python -c "import matplotlib.pyplot as plt;plt.figure()"'):
     matplotlib.use('Agg')
 
-from . import *
+from .ahkab import __version__, new_op, new_dc, new_tran, new_ac, new_pss
+from .ahkab import new_symbolic, queue, run, new_x0, icmodified_x0
+from .ahkab import get_op_x0, set_temperature, process_postproc, main
+
+__all__ = [__version__, new_op, new_dc, new_tran, new_ac, new_pss,
+           new_symbolic, queue, run, new_x0, icmodified_x0,
+           get_op_x0, set_temperature, process_postproc, main]
