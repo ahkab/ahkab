13488dbe8b99ddadaeaa4b2079bc3272c8be86fa
ggventurini <ggventurini+GITHUB@gmail.com>
2014-05-25 17:24:41 +0200

    Update rectifier test.

--- tests/rectifier/test_rectifier.py
+++ tests/rectifier/test_rectifier.py
@@ -8,19 +8,27 @@ def test():
 
     cir = ahkab.circuit.Circuit("Rectifier", filename=None)
     sinf = ahkab.devices.sin(0, 220, 50.)
-    cir.add_vsource('V1', 'inr', cir.gnd, 1., 0., sinf)
-    cir.add_resistor('R0', 'inr', 'in', .001)
-    cir.add_inductor('L1', 'in', cir.gnd, 100e-6)
-    cir.add_inductor('L2', 'int1', 'int2', 100e-6)
-    cir.add_inductor_coupling('K1', 'L1', 'L2', .04)
-    cir.add_model('diode', 'pnj', {'IS':1e-10})
+    cir.add_vsource('V1', 'inar', 'inbr', 1., 0., sinf)
+    cir.add_resistor('R0a', 'inar', 'ina', 1.)
+    cir.add_resistor('R0b', 'inbr', 'inb', .001)
+    cir.add_inductor('L1a', 'ina', cir.gnd, 100)
+    cir.add_inductor('L1b', cir.gnd, 'inb', 100)
+    cir.add_inductor('L2a', 'int1', cir.gnd, .5)
+    cir.add_inductor('L2b', cir.gnd, 'int2', .5)
+    cir.add_inductor_coupling('K1a', 'L1a', 'L2a', .98)
+    cir.add_inductor_coupling('K1b', 'L1b', 'L2b', .98)
+    cir.add_model('diode', 'pnj', {'IS':1e-9, 'RS':0})
     cir.add_diode('D1', 'int1', 'int4', 'pnj')
     cir.add_diode('D2', 'int3', 'int2', 'pnj')
     cir.add_diode('D3', 'int2', 'int4', 'pnj')
     cir.add_diode('D4', 'int3', 'int1', 'pnj')
     cir.add_capacitor('C1a', 'int3', 'int4', 100e-12)
-    cir.add_capacitor('C1b', 'int3', 'int4', 100e-6)
-    cir.add_resistor('R1', 'int3', 'int4', 1000)
+    cir.add_capacitor('C1b', 'int3', 'int4', 10e-6)
+    cir.add_resistor('R1', 'int3', 'int4', 1e4)
+    cir.add_resistor('R1d', 'int1', 'int4', 1e5)
+    cir.add_resistor('R2d', 'int3', 'int2', 1e5)
+    cir.add_resistor('R3d', 'int2', 'int4', 1e5)
+    cir.add_resistor('R4d', 'int3', 'int1', 1e5)
 
 
     if cli:
@@ -28,12 +36,16 @@ def test():
 
     ## define analyses
     op1 = ahkab.new_op(outfile='rectifier')
-    tran1 = ahkab.new_tran(0, 200e-3, 1e-3, outfile='rectifier')
+    tran1 = ahkab.new_tran(0, 200e-3, 1e-3, outfile='rectifier', 
+                           verbose=0+cli*6)
 
     ## create a testbench
     testbench = testing.APITest('rectifier', cir, [op1, tran1],
                                 skip_on_travis=False, ea=1e-1, er=1.)
 
+    ahkab.options.nl_voltages_lock = False
+    ahkab.options.nl_voltages_factor = 20
+    ahkab.options.iea = 1e-1
     ahkab.options.default_tran_method = "TRAP"
     ahkab.options.hmin = 1e-20
     ahkab.options.transient_max_nr_iter = 100
@@ -54,7 +66,7 @@ def test():
         ## plot and save interesting data
         fig = plt.figure()
         plt.title(cir.title + " inputs")
-        plt.plot(res['tran'].get_x(), res['tran']['VIN'], label='Input voltage')
+        plt.plot(res['tran'].get_x(), res['tran']['VINA']-res['tran']['VINB'], label='Transf. input')
         plt.hold(True)
         plt.plot(res['tran'].get_x(), res['tran']['vint1'], label='Transformer output #1')
         plt.plot(res['tran'].get_x(), res['tran']['vint2'], label='Transformer output #2')
