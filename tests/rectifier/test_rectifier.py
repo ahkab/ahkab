d5f7e9e63996ca29364c465b63453e80fc19f250
ggventurini <ggventurini+GITHUB@gmail.com>
2014-05-30 08:12:19 +0200

    Add RS, add assemble(), refresh circuit if cli.

--- tests/rectifier/test_rectifier.py
+++ tests/rectifier/test_rectifier.py
@@ -3,9 +3,8 @@ from ahkab import circuit, printing, devices, testing
 
 cli = False
 
-def test():
-    """Full wave rectifier test circuit"""
-
+def assemble():
+    RS = 1
     cir = ahkab.circuit.Circuit("Rectifier", filename=None)
     sinf = ahkab.devices.sin(0, 220, 50.)
     cir.add_vsource('V1', 'inar', 'inbr', 1., 0., sinf)
@@ -17,11 +16,19 @@ def test():
     cir.add_inductor('L2b', cir.gnd, 'int2', .5)
     cir.add_inductor_coupling('K1a', 'L1a', 'L2a', .98)
     cir.add_inductor_coupling('K1b', 'L1b', 'L2b', .98)
-    cir.add_model('diode', 'pnj', {'IS':1e-9, 'RS':0})
+    cir.add_model('diode', 'pnj', {'IS':1e-9, 'RS':1})
     cir.add_diode('D1', 'int1', 'int4', 'pnj')
     cir.add_diode('D2', 'int3', 'int2', 'pnj')
     cir.add_diode('D3', 'int2', 'int4', 'pnj')
     cir.add_diode('D4', 'int3', 'int1', 'pnj')
+#    cir.add_diode('D1', 'int1', 'int4f2', 'pnj')
+#    cir.add_diode('D2', 'int3', 'int2f', 'pnj')
+#    cir.add_diode('D3', 'int2', 'int4f', 'pnj')
+#    cir.add_diode('D4', 'int3', 'int1f', 'pnj')
+#    cir.add_resistor('R1D', 'int1f', 'int1', 1)
+#    cir.add_resistor('R2D', 'int2f', 'int2', 1)
+#    cir.add_resistor('R3D', 'int4f2', 'int4', 1)
+#    cir.add_resistor('R4D', 'int4f', 'int4', 1)
     cir.add_capacitor('C1a', 'int3', 'int4', 100e-12)
     cir.add_capacitor('C1b', 'int3', 'int4', 10e-6)
     cir.add_resistor('R1', 'int3', 'int4', 1e4)
@@ -30,14 +37,21 @@ def test():
     cir.add_resistor('R3d', 'int2', 'int4', 1e5)
     cir.add_resistor('R4d', 'int3', 'int1', 1e5)
 
-
     if cli:
         printing.print_circuit(cir)
+
+    return cir
+
+def test():
+    """Full wave rectifier test circuit"""
+
+    cir = assemble()
 
     ## define analyses
     op1 = ahkab.new_op(outfile='rectifier')
     tran1 = ahkab.new_tran(0, 200e-3, 1e-3, outfile='rectifier', 
                            verbose=0+cli*6)
+
 
     ## create a testbench
     testbench = testing.APITest('rectifier', cir, [op1, tran1],
@@ -48,7 +62,7 @@ def test():
     ahkab.options.iea = 1e-1
     ahkab.options.default_tran_method = "TRAP"
     ahkab.options.hmin = 1e-20
-    ahkab.options.transient_max_nr_iter = 100
+    ahkab.options.transient_max_nr_iter = 200
 
     ## setup and test
     testbench.setUp()
@@ -59,6 +73,7 @@ def test():
     ## for quick inspection
     if cli:
         ## re-run the test to grab the results
+        cir = assemble()
         res = ahkab.run(cir, an_list=[op1, tran1])
         # print-out for good measure
         print "OP Results:"
