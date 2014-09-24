81daa2d886141b90c03a105a328e6f58f22bd889
ggventurini <ggventurini+GITHUB@gmail.com>
2014-09-24 19:08:21 +0200

    TEST: add shooting test shooting1

---
+++ tests/shooting1/test_shooting1.py
@@ -0,0 +1,16 @@
+from ahkab.testing import NetlistTest
+from ahkab import options
+options.plotting_show_plots = False
+
+def test():
+    nt = NetlistTest('shooting1')
+    nt.setUp()
+    nt.test()
+    nt.tearDown()
+
+test.__doc__ = "PSS SHOOTING test 1"
+
+if __name__ == '__main__':
+    nt = NetlistTest('shooting1')
+    nt.setUp()
+    nt.test()
