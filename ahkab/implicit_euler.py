44879511784cace45b5bc476b09a906d310821f9
ggventurini <ggventurini+GITHUB@gmail.com>
2015-01-06 12:27:59 +0100

    Fix merge mistake.

--- ahkab/implicit_euler.py
+++ ahkab/implicit_euler.py
@@ -92,7 +92,7 @@ def get_df(pv_array, suggested_step, predict=True):
     x_lte_coeff = 0.5 * suggested_step
 
     if predict and len(pv_array) > 1 and pv_array[1][1] is not None:
-        predict = numpy.mat(numpy.zeros(pv_array[0][1].shape))
+        predict = np.mat(np.zeros(pv_array[0][1].shape))
         for index in range(predict.shape[0]):
             predict[index, 0] = (pv_array[0][1][index, 0] - pv_array[1][1][index, 0]) \
                 / (pv_array[0][0] - pv_array[1][0]) * suggested_step + pv_array[0][1][index, 0]
