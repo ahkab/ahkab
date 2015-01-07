140f5d5a2e28e48fe3419650b812ee14eda213cf
ggventurini <ggventurini+GITHUB@gmail.com>
2015-01-07 12:26:00 +0100

    Add explicit support for python3

--- setup.py
+++ setup.py
@@ -44,6 +44,8 @@ setup(
         "Operating System :: MacOS",
         "Natural Language :: English",
         "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7"]
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4"]
 )
 
