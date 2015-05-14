c4ed8696964eb65d756860c37a1b6edb7ff1cf83
ggventurini <ggventurini+GITHUB@gmail.com>
2015-05-14 10:04:10 +0200

    DOC: imporvements

--- ahkab/options.py
+++ ahkab/options.py
@@ -19,6 +19,8 @@
 
 """This module contains options and configuration switches the user
 may tune to meet his needs.
+
+The default values are sensible options for the general case.
 """
 
 from __future__ import (unicode_literals, absolute_import,
@@ -36,26 +38,25 @@ except ImportError:
 #: Encoding of the netlist files.
 encoding = 'utf8'
 
-#: Cache size to be used in :func:`utilities.memoize`, defaults to 512MB
+#: Cache size to be used in :func:`ahkab.utilities.memoize`, defaults to 512MB
 cache_len = 67108864 # 512MB
 
 #: A boolean to differentiate command line execution from module import
-#:
 #: When cli is False, no printing and no weird stdout stuff.
 cli = False
 
 # global: errors
-#: Voltage absolute tolerance
+#: Voltage absolute tolerance.
 vea = 1e-6
-#: Voltage relative tolerance
+#: Voltage relative tolerance.
 ver = 1e-3
-#: Current absolute tolerance
+#: Current absolute tolerance.
 iea = 1e-9
-#: Current relative tolerance
+#: Current relative tolerance.
 ier = 1e-3
 
 # global: circuit
-#: Minimum conductance to ground
+#: Minimum conductance to ground.
 gmin = 1e-12
 #: Should we show to the user results pertaining to nodes introduced by
 #: components or by the simulator?
@@ -63,7 +64,7 @@ print_int_nodes = True
 
 # global: solving
 #: Dense matrix limit: if the dimensions of the square MNA matrix are bigger,
-#: use sparse matrices
+#: use sparse matrices.
 dense_matrix_limit = 400
 #: Should we damp artificially the first NR iterations? See also
 #: :func:`ahkab.dc_analysis.get_td`.
@@ -75,7 +76,7 @@ nl_voltages_lock = True     # Apply damping - slows down solution.
 #: if we allow the voltage on controlling ports to change too much, we may
 #: have current/voltage overflows. Think about the diode characteristic.
 #: So we allow them to change of ``nl_voltages_lock_factor``
-#: :math:`{\\cdot V_{th}` at most and damp all variables accordingly.
+#: :math:`\cdot V_{th}` at most and damp all variables accordingly.
 nl_voltages_lock_factor = 4
 
 #: Whether the standard solving method can be used.
@@ -91,40 +92,43 @@ print_suppress = False
 print_precision = np.get_printoptions()['precision']
 
 # dc
-#: Maximum allowed NR iterations during a DC analysis
+#: Maximum allowed NR iterations during a DC analysis.
 dc_max_nr_iter = 10000
-#: Enable duessing to init the NR solver during a DC analysis
+#: Enable guessing to init the NR solver during a DC analysis.
 dc_use_guess = True
 #: Do not perform an init DC guess if its effort is higher than
 #: this value.
 dc_max_guess_effort = 250000
+# shorthand to set logarithmic stepping in DC analyses.
 dc_log_step = 'LOG'
+# shorthand to set linear stepping in DC analyses.
 dc_lin_step = 'LIN'
 #: Can we skip troublesome points during DC sweeps?
 dc_sweep_skip_allowed = True
 
 # transient
-#: The default differentiation method for transient analyses
+#: The default differentiation method for transient analyses.
 default_tran_method = "TRAP"
-#: Minimum allowed discretization step for time
+#: Minimum allowed discretization step for time.
 hmin = 1e-20
 #: Maximum number of time iterations for transient analyses
 #: Notice the default (0) means no limit is enforced.
 transient_max_time_iter = 0  # disabled
-#: Maximum number of NR iterations for transient analyses
+#: Maximum number of NR iterations for transient analyses.
 transient_max_nr_iter = 20
 #: In a transisent analysis, if a prediction value is avalilable,
 #: use it as first guess for ``x(n+1)``, otherwise ``x(n)`` is used.
 transient_prediction_as_x0 = True
-#: Use aposteriori step control
+#: Use aposteriori step control?
 transient_use_aposteriori_step_control = True
 #: Step change threshold:
-# we do not want to redo the iteraction if the aposteriori check suggests a step that is
-# very close to the one we already used. 0.9 seems to be a good idea.
+#: we do not want to redo the iteraction if the aposteriori check suggests a step
+#: that is very close to the one we already used. A value of 0.9 seems to be a
+#: good idea.
 transient_aposteriori_step_threshold = 0.9
-#: Disable all step control in transient analyses
+#: Disable all step control in transient analyses.
 transient_no_step_control = False
-#: Minimum capacitance to ground
+#: Minimum capacitance to ground.
 cmin = 1e-18
 
 # pss
@@ -132,14 +136,14 @@ BFPSS = 'brute-force'
 SHOOTINGPSS = 'shooting'
 
 # shooting
-#: Default number of points for a shooting analysis
+#: Default number of points for a shooting analysis.
 shooting_default_points = 100
-#: Maximum number of NR iterations for shooting analyses
+#: Maximum number of NR iterations for shooting analyses.
 shooting_max_nr_iter = 10000
 
 # symbolic
 #: Enable the manual solver:
-#: solve the circuit equations one at a time as you might do "manually"
+#: solve the circuit equations one at a time as you might do "manually".
 symb_sympy_manual_solver = False
 #: Formulate the equations with conductances and at the last moment
 #: swap resistor symbols back in. It seems to make sympy play nicer.
@@ -149,13 +153,13 @@ symb_formulate_with_gs = False
 # ac
 ac_log_step = 'LOG'
 ac_lin_step = 'LIN'
-#: Maximum number of NR iterations for AC analyses
+#: Maximum number of NR iterations for AC analyses.
 ac_max_nr_iter = 20
 #: Use degrees instead of rads in AC phase results.
 ac_phase_in_deg = False
 
 #pz
-#: maximum considered angular frequency in rad/s for PZ analyses
+#: Maximum considered angular frequency in rad/s for PZ analyses.
 pz_max = 1e12
 
 # plotting
@@ -172,7 +176,7 @@ plotting_wait_after_plot = True
 plotting_outtype = "png"
 #: Matplotlib line plot style: see matplotlib's doc.
 plotting_style = "-o"
-#: Plotting line width
+#: Plotting line width.
 plotting_lw = 1.25
 #: Default size for plots showed to the user, in inches.
 plotting_display_figsize = (12.94, 8)
