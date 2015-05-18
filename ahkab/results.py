e2abc6d674f6989dc217f02aa742d6d68b8f1430
ggventurini <ggventurini+GITHUB@gmail.com>
2015-05-18 17:50:12 +0200

    MINOR: re-add printing of the total power consumption after OPs

--- ahkab/results.py
+++ ahkab/results.py
@@ -467,6 +467,7 @@ class op_solution(solution, _mutable_data):
         for k in sorted(self._op_info.keys()):
             fp.write(printing.table(self._op_info[k], headers=self._op_keys[k][0]))
             fp.write('\n\n')
+        fp.write('Total power dissipation: %g W\n\n' % self.tot_power)
         #for opi in self.op_info:
         #    fp.write(opi)
         #    fp.write("-------------------\n")
